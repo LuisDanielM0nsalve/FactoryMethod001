dc8fbccc591ba2642e4cc9f6e22042e562edf0d2
LuisDanielM0nsalve <142621618+LuisDanielM0nsalve@users.noreply.github.com>
2024-08-25 21:14:46 -0500

    Music

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//public enum MusicFormat
+//{
+//    MP3,
+//    WAV,
+//    FLAC
+//}
+
+//public class MusicPlayer
+//{
+//    public void PlayMusic(MusicFormat format)
+//    {
+//        switch (format)
+//        {
+//            case MusicFormat.MP3:
+//                Console.WriteLine(
+//                    "reproduciendo archivo MP3..."
+//                    );
+//                break;
+//            case MusicFormat.WAV:
+//                Console.WriteLine(
+//                    "Reproduciendo archivo WAV..."
+//                     );
+//                break;
+//            case MusicFormat.FLAC:
+//                Console.WriteLine(
+//                    "Reproduciendo archivo FLAC..."
+//                    );
+//                break;
+//            default:
+//                Console.WriteLine(
+//                    "Formato no soportado");
+//                break;
+
+//        }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        MusicPlayer player = new MusicPlayer();
+//        player.PlayMusic(MusicFormat.MP3);
+//    }
+//}
---
+++ Program00.cs
@@ -0,0 +1,96 @@
+﻿namespace MusicPlayerApp
+{
+    public interface IMusicPlayer
+    {
+        void Play();
+    }
+
+    public class Mp3Player : IMusicPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("Reproduciendo archivo MP3...");
+        }
+    }
+
+    public class WavPlayer : IMusicPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("Reproduciendo archivo WAV...");
+        }
+    }
+
+    public class FlacPlayer : IMusicPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("Reproduciendo archivo FLAC...");
+        }
+    }
+    public class Mp4Player : IMusicPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("Reproduciendo archivo MP4...");
+        }
+    }
+
+    public abstract class MusicPlayerFactory
+    {
+        public abstract IMusicPlayer CreateMusicPlayer();
+    }
+
+    public class Mp3PlayerFactory : MusicPlayerFactory
+    {
+        public override IMusicPlayer CreateMusicPlayer()
+        {
+            return new Mp3Player();
+        }
+    }
+
+    public class WavPlayerFactory : MusicPlayerFactory
+    {
+        public override IMusicPlayer CreateMusicPlayer()
+        {
+            return new WavPlayer();
+        }
+    }
+
+    public class FlacPlayerFactory : MusicPlayerFactory
+    {
+        public override IMusicPlayer CreateMusicPlayer()
+        {
+            return new FlacPlayer();
+        }
+    }
+    public class Mp4PlayerFactory : MusicPlayerFactory
+    {
+        public override IMusicPlayer CreateMusicPlayer()
+        {
+            return new Mp4Player();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicPlayerFactory mp3Factory = new Mp3PlayerFactory();
+            IMusicPlayer mp3Player = mp3Factory.CreateMusicPlayer();
+            mp3Player.Play();
+
+            MusicPlayerFactory wavFactory = new WavPlayerFactory();
+            IMusicPlayer wavPlayer = wavFactory.CreateMusicPlayer();
+            wavPlayer.Play();
+
+            MusicPlayerFactory flacFactory = new FlacPlayerFactory();
+            IMusicPlayer flacPlayer = flacFactory.CreateMusicPlayer();
+            flacPlayer.Play();
+
+            MusicPlayerFactory mp4Factory = new Mp4PlayerFactory();
+            IMusicPlayer mp4Player = mp4Factory.CreateMusicPlayer();
+            mp4Player.Play();
+        }
+    }
+}
